c382240ff36e78411ed2e828488eacbaf494326a
Mallory Johnson <mjohnson4685@gmail.com>
2021-06-18 01:04:38 -0700

    commit tree 6e75653560f80470efe7723d297669c794f42f31

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+#name = "Matt"
+
+ def badge_maker(name)
+
+    "Hello, my name is #{name}."
+
+ end
+
+
+ def batch_badge_creator(array)
+    badge_array = []
+    array.each do |name|
+      badge_array << "Hello, my name is #{name}."
+    end
+    return badge_array
+  end
+
+
+
+  def assign_rooms(array)
+   
+    array.collect.each_with_index {|item, index|
+    "Hello, #{item}! You'll be assigned to room #{index + 1}!"}
+
+  end
+
+
+
+def printer(attendees)
+
+
+
+end
+
